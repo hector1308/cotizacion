2d6d2194aadf0bea3df81c2b9f90c0fe57fa6048
hector1308 <hectorhoracioaliende@gmail.com>
2025-09-29 23:37:05 -0300

    Ruta principal apunta a CotizacionController

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CotizacionController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-// Vista web de cotizaciones
-Route::get('/cotizacion', [CotizacionController::class, 'vistaCotizacion'])->name('cotizacion.vista');
+// Ruta principal apuntando al dashboard de cotizaciones
+Route::get('/', [CotizacionController::class, 'vistaCotizacion'])->name('cotizacion.vista');
 
 // Endpoint JSON para recargar tabla con JS
 Route::get('/cotizacion/json', [CotizacionController::class, 'jsonCotizaciones']);
